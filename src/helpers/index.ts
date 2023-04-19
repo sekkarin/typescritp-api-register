8960d326447e4e5b23f8381b6f3428990811c3e9
sekkarin <sekkri1234@gmail.com>
2023-04-19 20:17:57 +0700

    ...

--- src/helpers/index.ts
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto'; // Encrypt decode  hash ...
-
-export const random =()=> crypto.randomBytes(128).toString('base64');
-
-export const authentication = (salt: string, password: string) => {
-    return crypto.createHmac('sha256', [salt, password].join('/'))
-    .update(process.env.SECRET as string).digest('hex');
-}
