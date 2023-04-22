5f0a0870fd6028c0adbba3d5330fef1f0412d647
sekkarin <sekkri1234@gmail.com>
2023-04-22 22:23:59 +0700

    ceate login

--- src/controllers/authenticationControtroller.ts
+++ src/controllers/authenticationControtroller.ts
@@ -1,9 +1,8 @@
 import { log } from 'console';
-// import { createUser } from '../db/user';
 import { getUserByEmail,createUser } from '../db/user';
 import express, { Request, Response, NextFunction } from 'express';
 import { authentication,random } from '../helpers';
-// import { random } from '../helpers';
+
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -28,6 +27,41 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
             }
         })
         return res.status(200).json(user).end()
+    } catch (error) {
+        log(error)
+        res.sendStatus(400)
+    }
+}
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    
+    try {
+        const { email, password } = req.body
+        // log(email, password )
+        //? ตรวจสอบข้อมูลว่ามีมั้ย
+        if (!email || !password  ) res.sendStatus(400)
+        
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+
+        // ? ตรวจสอบว่ามี user หรือไม่?
+        if(!user) res.sendStatus(400)
+
+        if(!user?.authentication?.salt || !user?.authentication?.password) {
+            return res.sendStatus(400)
+        }
+
+        const expectedHash = authentication(user?.authentication.salt ,password)
+
+        if(user.authentication.password !== expectedHash) res.sendStatus(403)
+
+
+        const salt = random()
+        user.authentication.sessionToken = authentication(salt,user._id.toString())
+        await user.save()
+
+        res.cookie("TEST_AUTH",user.authentication.sessionToken,{domain:'localhost',path:'/'})
+        return res.status(200).json(user)
+
     } catch (error) {
         log(error)
         res.sendStatus(400)
